2305c2652ccf8372c40c1695bc62e539cf2614c2
yeryeong0519 <yeryeong0519@gmail.com>
2024-05-23 17:49:53 +0900

    .

---
+++ src/views/PostsByTagView.vue
@@ -0,0 +1,15 @@
+<script setup>
+import PostList from "../components/PostList.vue";
+</script>
+
+<template>
+	<h2>Posts by Tag</h2>
+	<PostList />
+</template>
+
+<style scoped>
+	h2{
+		color : green;
+	}
+</style>
+
